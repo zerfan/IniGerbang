e2621012a33d5fcb2af4386eba1111cd36f72a25
zerfan <50097654+zerfan@users.noreply.github.com>
2020-11-05 13:24:14 +0800

    Add files via upload

--- inigerbangv2beta/webif.sql
+++ inigerbangv2beta/webif.sql
@@ -1,1 +1,272 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 05, 2020 at 06:23 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webif`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dosen`
+--
+
+CREATE TABLE `dosen` (
+  `id` int(11) NOT NULL,
+  `nip` varchar(100) NOT NULL,
+  `nama_dosen` varchar(100) NOT NULL,
+  `id_prodi` int(11) NOT NULL,
+  `pendidikan` varchar(100) NOT NULL,
+  `alamat` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dosen`
+--
+
+INSERT INTO `dosen` (`id`, `nip`, `nama_dosen`, `id_prodi`, `pendidikan`, `alamat`) VALUES
+(1, '11181033', 'Ph.D. Hangga Tri Irawan', 11, 'S3', 'Jalan Orang Muda RT 29 NO 48'),
+(2, '11181057', 'Ph.D. M. Khoirul A.', 11, 'S3', 'Jalan Orang Tua RT 8 NO 9');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `frs`
+--
+
+CREATE TABLE `frs` (
+  `id_frs` int(11) NOT NULL,
+  `id_mahasiswa` int(11) NOT NULL,
+  `id_matkul` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nim` varchar(100) NOT NULL,
+  `id_prodi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nim`, `id_prodi`) VALUES
+(1, 'Bagus ', '111802', 11),
+(2, 'Abdi', '111801', 11);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matkul`
+--
+
+CREATE TABLE `matkul` (
+  `id` int(11) NOT NULL,
+  `nama_mk` varchar(100) NOT NULL,
+  `sks` int(3) NOT NULL,
+  `kode` varchar(100) NOT NULL,
+  `id_dosen` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `matkul`
+--
+
+INSERT INTO `matkul` (`id`, `nama_mk`, `sks`, `kode`, `id_dosen`) VALUES
+(8, 'Algoritma Pemrograman 1', 3, 'IF001', 1),
+(9, 'Algoritma Pemrograman 2', 3, 'IF002', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prodi`
+--
+
+CREATE TABLE `prodi` (
+  `id_prodi` int(11) NOT NULL,
+  `nama_prodi` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `prodi`
+--
+
+INSERT INTO `prodi` (`id_prodi`, `nama_prodi`) VALUES
+(1, 'Fisika'),
+(2, 'Matematika'),
+(3, 'Teknik Mesin'),
+(4, 'Teknik Elektro'),
+(5, 'Teknik Kimia'),
+(6, 'Teknik Material dan Metalurgi'),
+(7, 'Teknik Sipil'),
+(8, 'Perencanaan Wilayah dan Kota'),
+(9, 'Teknik Perkapalan'),
+(10, 'Sistem Informasi'),
+(11, 'Informatika'),
+(12, 'Teknik Industri'),
+(13, 'Teknik Lingkungan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dosen`
+--
+ALTER TABLE `dosen`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nip` (`nip`),
+  ADD KEY `id_prodi_dosen` (`id_prodi`);
+
+--
+-- Indexes for table `frs`
+--
+ALTER TABLE `frs`
+  ADD PRIMARY KEY (`id_frs`),
+  ADD KEY `id_mhs` (`id_mahasiswa`),
+  ADD KEY `id_mk` (`id_matkul`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nim` (`nim`),
+  ADD KEY `id_prodi_mhs` (`id_prodi`);
+
+--
+-- Indexes for table `matkul`
+--
+ALTER TABLE `matkul`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `kode` (`kode`),
+  ADD KEY `id_dosen_mk` (`id_dosen`);
+
+--
+-- Indexes for table `prodi`
+--
+ALTER TABLE `prodi`
+  ADD PRIMARY KEY (`id_prodi`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dosen`
+--
+ALTER TABLE `dosen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `frs`
+--
+ALTER TABLE `frs`
+  MODIFY `id_frs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `matkul`
+--
+ALTER TABLE `matkul`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `prodi`
+--
+ALTER TABLE `prodi`
+  MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `dosen`
+--
+ALTER TABLE `dosen`
+  ADD CONSTRAINT `id_prodi_dosen` FOREIGN KEY (`id_prodi`) REFERENCES `prodi` (`id_prodi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `frs`
+--
+ALTER TABLE `frs`
+  ADD CONSTRAINT `id_mhs` FOREIGN KEY (`id_mahasiswa`) REFERENCES `mahasiswa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_mk` FOREIGN KEY (`id_matkul`) REFERENCES `matkul` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD CONSTRAINT `id_prodi_mhs` FOREIGN KEY (`id_prodi`) REFERENCES `prodi` (`id_prodi`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `matkul`
+--
+ALTER TABLE `matkul`
+  ADD CONSTRAINT `id_dosen` FOREIGN KEY (`id_dosen`) REFERENCES `dosen` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
